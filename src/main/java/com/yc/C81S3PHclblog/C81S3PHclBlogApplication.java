be7759ca43afc49c4e82ed990b6186fbf3da3c3a
hcl0923 <2958731272@qq.com>
2020-09-16 10:41:56 +0800

    git

--- src/main/java/com/yc/C81S3PHclblog/C81S3PHclBlogApplication.java
+++ src/main/java/com/yc/C81S3PHclblog/C81S3PHclBlogApplication.java
@@ -1,5 +1,7 @@
 package com.yc.C81S3PHclblog;
  
+
+
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
