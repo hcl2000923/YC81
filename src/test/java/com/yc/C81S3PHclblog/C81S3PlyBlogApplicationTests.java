cd011713e9241aaa9450b91b436c4dfd25cfce98
hcl0923 <2958731272@qq.com>
2020-09-20 15:16:23 +0800

    3

---
+++ src/test/java/com/yc/C81S3PHclblog/C81S3PlyBlogApplicationTests.java
@@ -0,0 +1,54 @@
+package com.yc.C81S3PHclblog;
+
+import javax.annotation.Resource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+import com.yc.C81S3PHclblog.bean.Category;
+import com.yc.C81S3PHclblog.bean.User;
+import com.yc.C81S3PHclblog.biz.BizException;
+import com.yc.C81S3PHclblog.biz.UserBiz;
+import com.yc.C81S3PHclblog.dao.CategoryMapper;
+
+@SpringBootTest
+class C81S3PlyBlogApplicationTests {
+
+	@Resource
+	private CategoryMapper cm;
+	
+	@Resource
+	private UserBiz ub;
+
+	@Test
+	void contextLoads() {
+		for (Category c : cm.selectAll()) {
+			System.out.println(c.getName());
+		}
+	}
+	@Test
+	void test() {
+		User user = new User();
+		user.setAccount("zhangsan");
+		user.setPwd("123456");
+
+		try {
+			User dbuser = ub.login(user);
+			Assert.notNull(dbuser, "dbuser不能为空");
+		} catch (BizException e) {
+			e.printStackTrace();
+		}
+
+		user.setPwd("654321");
+		try {
+			User dbuser = ub.login(user);
+			Assert.isNull(dbuser, "dbuser必须为空");
+		} catch (BizException e) {
+			e.printStackTrace();
+			Assert.hasText(e.getMessage(), "用户名或密码错误");
+		}
+
+	}
+	
+}
